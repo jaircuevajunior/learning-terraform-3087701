9e1c83cd7fa5e51381be3ea2203fcd2eefdb3765
Jair Cueva Júnior <junior@dinhost.com.br>
2023-12-23 18:04:16 -0300

    Associate public ip address into ec2

--- main.tf
+++ main.tf
@@ -37,8 +37,9 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id = module.blog_vpc.public_subnets[0]
+  vpc_security_group_ids      = [module.blog_sg.security_group_id]
+  subnet_id                   = module.blog_vpc.public_subnets[0]
+  associate_public_ip_address = true
 
   tags = {
     Name = "HelloWorld"
