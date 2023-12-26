709ed09cc4de2182c6fa402f49c92e786d59dbef
Jair Cueva Júnior <junior@dinhost.com.br>
2023-12-26 17:47:10 -0300

    debug

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "instance_ids" {
-    description = "id das instancias ec2"
-    value = data.aws_instances.blog_instances[0].id
-}
-
 output "instance_ids2" {
     description = "id das instancias ec2 single"
     value = data.aws_instances.blog_instances.id
