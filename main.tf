65fda0c30e3eb0331c31f2b8a502d5461dc4d5af
Jair Cueva Júnior <junior@dinhost.com.br>
2023-12-26 10:26:23 -0300

    ASG

--- main.tf
+++ main.tf
@@ -33,18 +33,110 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
 
-  vpc_security_group_ids      = [module.blog_sg.security_group_id]
-  subnet_id                   = module.blog_vpc.public_subnets[0]
-  associate_public_ip_address = true
+
+module "blog_asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+
+  # Autoscaling group
+  name = "blog_asg"
+
+  min_size                  = 1
+  max_size                  = 2
+  desired_capacity          = 2
+  wait_for_capacity_timeout = 0
+  health_check_type         = "EC2"
+  vpc_zone_identifier       = module.blog_vpc.public_subnets
+
+  # Launch template
+  #launch_template_name        = "example-asg"
+  #launch_template_description = "Launch template example"
+  #update_default_version      = true
+
+  image_id          = data.aws_ami.app_ami.id
+  instance_type     = "t3a.micro"
+  ebs_optimized     = true
+  enable_monitoring = true
+
+  # IAM role & instance profile
+  create_iam_instance_profile = true
+  iam_role_name               = "example-asg"
+  iam_role_path               = "/ec2/"
+  iam_role_description        = "IAM role example"
+  iam_role_tags = {
+    CustomIamRole = "Yes"
+  }
+  iam_role_policies = {
+    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
+
+  block_device_mappings = [
+    {
+      # Root volume
+      device_name = "/dev/xvda"
+      no_device   = 0
+      ebs = {
+        delete_on_termination = true
+        encrypted             = true
+        volume_size           = 20
+        volume_type           = "gp2"
+      }
+    }, {
+      device_name = "/dev/sda1"
+      no_device   = 1
+      ebs = {
+        delete_on_termination = true
+        encrypted             = true
+        volume_size           = 30
+        volume_type           = "gp2"
+      }
+    }
+  ]
+
+
+  # This will ensure imdsv2 is enabled, required, and a single hop which is aws security
+  # best practices
+  # See https://docs.aws.amazon.com/securityhub/latest/userguide/autoscaling-controls.html#autoscaling-4
+  metadata_options = {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  }
+
+  network_interfaces = [
+    {
+      delete_on_termination = true
+      description           = "eth0"
+      device_index          = 0
+      security_groups       = [module.blog_sg.security_group_id]
+    }
+  ]
+
+  placement = {
+    availability_zone = "us-west-1a"
+  }
+
+  tag_specifications = [
+    {
+      resource_type = "instance"
+      tags          = { WhatAmI = "Instance" }
+    },
+    {
+      resource_type = "volume"
+      tags          = { WhatAmI = "Volume" }
+    },
+    {
+      resource_type = "spot-instances-request"
+      tags          = { WhatAmI = "SpotInstanceRequest" }
+    }
+  ]
 
   tags = {
-    Name = "HelloWorld"
+    Environment = "dev"
   }
 }
+
+
 
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
