19389396a546a48a0c5f8d94b4b0c286ef36bfff
Jair Cueva Júnior <junior@dinhost.com.br>
2023-12-27 14:31:13 -0300

    fix typo

--- qa/main.tf
+++ qa/main.tf
@@ -1,12 +1,12 @@
 module "qa" {
     source = "../modules/blog"
 
-    env {
+    env = {
         name           = "qa"
         network_prefix = "10.1"
     }
 
-    asg_settings {
+    asg_settings = {
         min_size         = 1
         max_size         = 1
         desired_capacity = 1
