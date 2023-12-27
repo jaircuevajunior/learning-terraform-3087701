dfbbf2883f37fc97df3d706afb1310a6723adc7b
Jair Cueva Júnior <junior@dinhost.com.br>
2023-12-27 14:23:10 -0300

    define qa env

---
+++ qa/main.tf
@@ -0,0 +1,14 @@
+module "qa" {
+    source = "../modules/blog"
+
+    env {
+        name           = "qa"
+        network_prefix = "10.1"
+    }
+
+    asg_settings {
+        min_size         = 1
+        max_size         = 1
+        desired_capacity = 1
+    }
+}
---
+++ qa/outputs.tf
@@ -0,0 +1,4 @@
+output "environment_url" {
+    description = "The URL of the ALB"
+    value = module.qa.environment_url
+}
